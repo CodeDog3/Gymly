ad22b671a4cfd78edac62f2c46f74e98e6085dbf
Jon Brennan <jonbrennan800@gmail.com>
2024-01-29 20:19:58 -0500

    item deletion functionality

--- app/home/builder/_components/ActiveExerciseList.tsx
+++ app/home/builder/_components/ActiveExerciseList.tsx
@@ -1,4 +1,5 @@
 import React, { PropsWithChildren, SetStateAction } from 'react'
+import { IoCloseSharp } from "react-icons/io5";
 
 type Props = {
     name: string,
@@ -15,47 +16,58 @@ type Dispatch = {
     }[]>>
 }
 
-const ActiveExerciseList = ({name, sets, reps, children, ExerciseIndex, dispatch }: PropsWithChildren<Props> & Dispatch) => {
+const ActiveExerciseList = ({ name, sets, reps, children, ExerciseIndex, dispatch }: PropsWithChildren<Props> & Dispatch) => {
 
-    const handleChange = (e:React.ChangeEvent<HTMLInputElement>, idx:number, type:string) =>{
-        let value:number;
-        try{
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>, idx: number, type: string) => {
+        let value: number;
+        try {
             value = parseInt(e.target.value);
         }
-        catch(e){
+        catch (e) {
             console.log(e)
         }
 
-        if(type === "sets" && e.target.value!=null){[
+        if (type === "sets" && e.target.value != null) {
+            [
+                dispatch(state => {
+                    const newState = Array.from(state);
+                    newState[idx].sets = value;
+                    return newState;
+                })
+            ]
+        }
+        if (type === "reps" && e.target.value != null) {
             dispatch(state => {
-                const newState = state;
-                newState[idx].sets = value;
-                return newState;
-            })
-        ]}
-        if(type === "reps" && e.target.value != null){
-            dispatch(state =>{
-                const newState = state;
+                const newState = Array.from(state);
                 newState[idx].reps = value
                 return newState
             })
         }
     }
 
-  return (
-    <div 
-    className='w-full py-4 bg-slate-400 rounded-md flex justify-evenly'>
-        <div>{name} </div>
-        <div>
-        <label>Sets</label>
-        <input className='w-6' type='number' required onChange={(e) => handleChange(e, ExerciseIndex, "sets")}></input>
+    const onClickHandler = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>,  ExerciseIndex:number) =>{
+        dispatch(state => {
+            const newStates = Array.from(state);
+            newStates.splice(ExerciseIndex,1)
+            return newStates
+    })
+    }
+
+    return (
+        <div
+            className='w-full py-4 bg-slate-400 rounded-md flex justify-evenly'>
+            <div>{name} </div>
+            <div>
+                <label>Sets</label>
+                <input className='w-6' type='number' required onChange={(e) => handleChange(e, ExerciseIndex, "sets")}></input>
+            </div>
+            <div>
+                <label>Reps</label>
+                <input className='w-6' type='number' required onChange={(e) => handleChange(e, ExerciseIndex, "reps")}></input>
+            </div>
+            <button onClick={(e)=>onClickHandler(e,ExerciseIndex)}><IoCloseSharp size={"1.5rem"}/></button>
         </div>
-        <div>
-        <label>Reps</label>
-        <input className='w-6' type='number' required onChange={(e) => handleChange(e, ExerciseIndex, "reps")}></input>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default ActiveExerciseList
--- app/home/builder/_components/Dropper.tsx
+++ app/home/builder/_components/Dropper.tsx
@@ -21,10 +21,9 @@ const Dropper = () => {
     const handleDragEnd = (result: any)=> {
       if(!result.destination) return;
       const items = Array.from(dropzone);
-      const [...reorderedItem] = items.splice(result.source.index, 1);
-      items.splice(result.destination.index,0,...reorderedItem);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index,0,reorderedItem);
       setDropzone(items)
-      console.log(dropzone);
     }
 
     return (
@@ -33,11 +32,11 @@ const Dropper = () => {
           <DragDropContext onDragEnd={handleDragEnd}>
             <Droppable droppableId='exercise-item'>
               {(provided) => (
-          <ul onDrop={handleDrop} onDragOver={handleDragOver} {...provided.droppableProps} ref={provided.innerRef} className='border-2 h-[40%] w-[40%] border-green-600 flex flex-col gap-y-1'>
+          <ul onDrop={handleDrop} onDragOver={handleDragOver} {...provided.droppableProps} ref={provided.innerRef} className='border-2 min-h-[40%] w-[40%] border-green-600 flex flex-col gap-y-1'>
             {dropzone.map((item, idx) => (
               <Draggable key={idx} draggableId={idx.toString()} index={idx}>
                 {(provided) => (
-                  <li {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}>
+                  <li {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps} >
                     <ActiveExerciseList  ExerciseIndex={idx} name={item.name} sets={item.sets} reps={item.reps} dispatch={setDropzone} />
                   </li>
                 )}
